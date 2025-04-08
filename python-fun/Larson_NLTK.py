4f777bcec7520babfc4bc705220d7aa12dedd60f
EvLar64 <eml5823@psu.edu>
2025-04-08 12:27:35 -0400

    more edits for python

--- python-fun/Larson_NLTK.py
+++ python-fun/Larson_NLTK.py
@@ -1,6 +1,5 @@
 import os
 from nltk.corpus import wordnet as wn
-cwd = os.getcwd()
 print()
 print("Python/NLTK Assignment (w/ POS) - EL")
 print()
@@ -8,9 +7,8 @@ print("Sample of Synsets on the word 'take' (smoke test)")
 print()
 for synset in wn.synsets('take'):
     print(synset.lemma_names(), len(synset.lemma_names()))
-
-filepath = '../breakfast.txt'
-print(filepath)
+cwd = os.getcwd()
+filepath = '/evSite/hughesColl/breakfast.txt'
 f = open(filepath, 'r', encoding='utf8').read()
 print(f)
 
@@ -18,6 +16,10 @@ os.listdir(cwd)
 coll = os.path.join(cwd, 'hughesColl')
 os.listdir(coll)
 
+for file in os.listdir(coll):
+   if file.endswith(".txt"):
+        filepath = f"{coll}/{file}"
+        print(filepath)
 from nltk.corpus import PlaintextCorpusReader
 corpus_root = 'hughesColl'
 corpus = PlaintextCorpusReader(corpus_root, '.*')
